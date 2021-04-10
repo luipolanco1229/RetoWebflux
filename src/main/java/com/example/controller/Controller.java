63ffdefa4c9cbd2913a4281a829d3322632c142c
luipolanco1229 <Luipolanco1229>
2021-04-10 15:28:48 -0500

    Se hicieron cambios en el fromIterable, se creo el segundo servicio reactivo.zip

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple3;
 
 @RestController
 public class Controller {
@@ -14,7 +15,13 @@ public class Controller {
 
     @GetMapping(value = "api/movies")
     public Flux<String> title(){
-        return service.title();
+        return service.fromIterable();
     }
+
+    @GetMapping(value = "api/movies/zip")
+    public Flux<Tuple3<String, String, String>> zip(){
+         return service.zipMethod();
+    }
+
 
 }
--- src/main/java/com/example/service/ServiceMovies.java
+++ src/main/java/com/example/service/ServiceMovies.java
@@ -1,15 +1,35 @@
 package com.example.service;
 
 import com.example.JSoupWeb;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuple3;
 
 @Service
 public class ServiceMovies {
 
+    private static final Logger Log = LoggerFactory.getLogger(ServiceMovies.class);
     JSoupWeb dataElements = new JSoupWeb();
 
-    public Flux<String> title () {
-        return Flux.fromIterable(dataElements.elementtitle());
+    public Flux<String> fromIterable () {
+        var movieslist = Flux.fromIterable(dataElements.titleList());
+        return movieslist;
     }
+
+    public Flux<Tuple3<String, String, String>> zipMethod(){
+        var movieslist = Flux.fromIterable(dataElements.titleList());
+        var genreList = Flux.fromIterable(dataElements.genreList());
+        var noteList = Flux.fromIterable(dataElements.noteList());
+
+       var zip = Flux.zip(movieslist, genreList, noteList);
+
+       return zip;
+
+
+    }
+
+
 }
